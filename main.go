b8177508e83ff2b0f5693bba37990e9fa16be035
c1337 <cyril199897@gmail.com>
2023-11-08 14:35:33 +0530

    basic framework added

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/c-m3-codin/red_sonto/message_proto"
+	"github.com/gin-gonic/gin"
+	"github.com/golang/protobuf/proto"
+)
 
 func main() {
 
@@ -13,4 +19,33 @@ func pongHandler(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "pong",
 	})
+}
+
+func protoSetHandler(c *gin.Context) {
+
+}
+
+func jsonSetHandler(c *gin.Context) {
+
+}
+
+func protoGetHandler(c *gin.Context) {
+
+}
+
+func jsonGetHandler(c *gin.Context) {
+
+}
+
+func setProto(val string) (data []byte, err error) {
+	// var text = []byte(val)
+	message := &message_proto.Message{
+		Text: val,
+	}
+	data, err = proto.Marshal(message)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(data)
+	return
 }
